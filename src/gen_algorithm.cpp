e31e984f870a9a97560b0902b7b1a44690342111
kowusek <kowusek@gmail.com>
2020-11-20 17:32:59 +0100

    fixed gen_function.

--- src/gen_algorithm.cpp
+++ src/gen_algorithm.cpp
@@ -8,7 +8,7 @@ gen_algorithm::~gen_algorithm() {
 
 }
 
-void gen_function(int population_size, float mutation_probability, float cross_probability) {
+void gen_algorithm::gen_function(int population_size, float mutation_probability, float cross_probability) {
 
 
 }
