4a01654b0748318ba2104630af871a0be84fff18
kowusek <kowusek@gmail.com>
2020-11-20 19:06:56 +0100

    added basic algorithm interface

--- src/gen_algorithm.cpp
+++ src/gen_algorithm.cpp
@@ -1,14 +1,27 @@
 #include "gen_algorithm.hpp"
 
-gen_algorithm::gen_algorithm() {
+gen_algorithm::gen_algorithm() = default;
+
+gen_algorithm::~gen_algorithm() = default;
+
+gen_algorithm::gen_algorithm(unsigned p_size, float m_probability, float c_probability, unsigned t) {
+
+    population_size = p_size;
+    mutation_probability = m_probability;
+    cross_probability = c_probability;
+    population = new std::vector<std::vector<unsigned>>;
+    best_so_far = NULL;
+    parm_t = t;
+}
+
+void gen_algorithm::gen_function() {
+
 
 }
 
-gen_algorithm::~gen_algorithm() {
+unsigned gen_algorithm::generate_number() {
 
-}
-
-void gen_algorithm::gen_function(int population_size, float mutation_probability, float cross_probability) {
-
-
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::mt19937 generator( seed );
+    return generator();
 }
