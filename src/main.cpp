95d21dc3c6bda50465023a6c8ce069aca346b24e
Jakub Rybalko <rybalkojakubpiotr@gmail.com>
2020-11-20 17:52:56 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -3,4 +3,6 @@
 int main() {
     std::cout << "Hello Easy C++ project!" << std::endl;
     std::cout << "It's mutation and cross!" << std::endl;
+    std::cout << "idk ti works!" << std::endl;
+
 }
